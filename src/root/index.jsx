108f9d9307b2b6895463085b6d79c40e44f2f765
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-11-24 08:09:06 +0530

    working infinite scroll

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 // styles
 import styles from './styles.module.css'
@@ -7,52 +8,45 @@ import styles from './styles.module.css'
 function App() {
 	const [userData, setUserData] = useState([])
 	const userRef = useRef(0)
+	const [visible, setVisible] = useState(false)
 
-	const FetchUserData = async (num) => {
-		let last_user = num * 10
-		let get_user = last_user - 9
-		for (get_user; get_user <= last_user; get_user++) {
-			const fetch_userData_url = `https://jsonplaceholder.typicode.com/posts/${get_user}`
+	const FetchUserData = async () => {
+		userRef.current = userRef.current + 1 //incrementing userRef
+		let last_user = userRef.current * 10
+		let crr_user = last_user - 9
+		for (crr_user; crr_user <= last_user; crr_user++) {
+			const fetch_userData_url = `https://jsonplaceholder.typicode.com/posts/${crr_user}`
 			try {
 				const { data } = await axios.get(fetch_userData_url)
 				setUserData((prev) => [...prev, data])
 			} catch (err) {
 				console.log(err)
 			}
+
+			setVisible(false)
 		}
 	}
 	useEffect(() => {
-		// to control the renders.
-		// the variables can't be used as the assignment to the variables inside the hook will be lost on every render.
-		// state can't be used as they show the previous value till the dom is renderd which is too late.
-		// hence, useRef cam out at top for showing true current state of the app
-
 		if (userRef.current === 0) {
-			FetchUserData(1)
-			userRef.current = 1
+			FetchUserData()
 		}
-
-		let options = {
-			// root: null,
-			// rootMargin: '0px',
-			// threshold: 1.0,
-		}
-
-		let callback = (entries, observer) => {
-			entries.forEach((entry, i) => {
-				console.log(entry.isIntersecting)
-			})
-		}
-
-		let observer = new IntersectionObserver(callback, options)
-		const intersecting_div = document.getElementById('intersector')
-		observer.observe(intersecting_div)
-	}, [userData])
+	}, [userData, visible])
 
 	return (
 		<div className='App'>
 			<h1>Request Users</h1>
-			<div className={styles.users_cont}>
+			<div className={styles.users_cont}></div>
+
+			<InfiniteScroll
+				dataLength={userData.length} //required field
+				next={userData.length === userRef.current * 10 && FetchUserData} //to ensure that data has been rendered before making a fetch request
+				hasMore={userRef.current < 10 ? true : false} //using userRef for checking the actual current fetch user data request
+				endMessage={
+					<p style={{ textAlign: 'center' }}>
+						<b>Yay! You have seen it all</b>
+					</p>
+				}
+			>
 				{userData.length > 0
 					? userData.map((user) => (
 							<div key={user.id}>
@@ -63,8 +57,7 @@ function App() {
 							</div>
 					  ))
 					: ''}
-			</div>
-			<div id='intersector'></div>
+			</InfiniteScroll>
 		</div>
 	)
 }
