180aba410d4f2c028c2c0f6e13f2452c35be7022
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-11-24 08:51:16 +0530

    switched to html-react-parser from react-html-parser

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 import InfiniteScroll from 'react-infinite-scroll-component'
-
+import parse from 'html-react-parser'
 // styles
 import styles from './styles.module.css'
 
@@ -12,13 +12,21 @@ function App() {
 
 	const FetchUserData = async () => {
 		userRef.current = userRef.current + 1 //incrementing userRef
+
 		let last_user = userRef.current * 10
 		let crr_user = last_user - 9
+
 		for (crr_user; crr_user <= last_user; crr_user++) {
 			const fetch_userData_url = `https://jsonplaceholder.typicode.com/posts/${crr_user}`
+
 			try {
 				const { data } = await axios.get(fetch_userData_url)
-				setUserData((prev) => [...prev, data])
+
+				let gender = crr_user % 2 === 0 ? 'male' : 'female'
+				const dicebar_avatar_url = `https://avatars.dicebear.com/api/${gender}/${data.title}.svg`
+
+				const { data: img } = await axios.get(dicebar_avatar_url)
+				setUserData((prev) => [...prev, { ...data, img }])
 			} catch (err) {
 				console.log(err)
 			}
@@ -41,19 +49,18 @@ function App() {
 				dataLength={userData.length} //required field
 				next={userData.length === userRef.current * 10 && FetchUserData} //to ensure that data has been rendered before making a fetch request
 				hasMore={userRef.current < 10 ? true : false} //using userRef for checking the actual current fetch user data request
-				endMessage={
-					<p style={{ textAlign: 'center' }}>
-						<b>Yay! You have seen it all</b>
-					</p>
-				}
+				endMessage={<p style={{ textAlign: 'center' }}>{userData.length === 100 ? <b>Yay! You have seen it all</b> : <b>More data loading</b>}</p>}
 			>
 				{userData.length > 0
 					? userData.map((user) => (
 							<div key={user.id}>
-								<p>{user.id}</p>
-								<p>{user.title}</p>
-								<p>{user.body}</p>
-								<p>{user.userId}</p>
+								<div>{parse(user.img)}</div>
+								<div>
+									<p>{user.id}</p>
+									<p>{user.title}</p>
+									<p>{user.body}</p>
+									<p>{user.userId}</p>
+								</div>
 							</div>
 					  ))
 					: ''}
